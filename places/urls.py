4b59b5e84f1f4b3325812fb534a9bfe7c9cbd9bb
Mengstu yaregal <mengstuyaregal9337@gmail.com>
2024-06-19 22:25:02 +0300

    user authentication

--- places/urls.py
+++ places/urls.py
@@ -1,10 +1,12 @@
-# places/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('place/<int:place_id>/', views.place_detail, name='place_detail'),
-    
     path('places/create/', views.create_place, name='create_place'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- places/views.py
+++ places/views.py
@@ -1,28 +1,70 @@
-# places/views.py
 from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Place
 from .forms import PlaceForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse, reverse_lazy
+from django.views import View
+
+
+class CustomLoginView(View):
+    template_name = 'places/login.html'
+    success_url = reverse_lazy('index')
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(self.success_url)
+        form = AuthenticationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(self.success_url)
+        return render(request, self.template_name, {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'places/register.html', {'form': form})
+
 
 def index(request):
     places = Place.objects.all()
     return render(request, 'places/index.html', {'places': places})
+
 
 @user_passes_test(lambda u: u.is_superuser)
 def create_place(request):
     if request.method == 'POST':
         form = PlaceForm(request.POST, request.FILES)
         if form.is_valid():
-            place = form.save()  # Save the form data to create a new Place object
-            return redirect('place_detail', place.id)  # Redirect to the detail view of the new place
+            place = form.save()
+            return redirect('place_detail', place.id)
     else:
         form = PlaceForm()
     
     return render(request, 'places/create_place.html', {'form': form})
-    
 
 
 def place_detail(request, place_id):
     place = Place.objects.get(id=place_id)
     return render(request, 'places/detail.html', {'place': place})
-
